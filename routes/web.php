dc83ff52928c9faa80304a0a29b2965f787f12c2
Ahmadou Bamba Thiam <thiamahmadoubamba863@gmail.com>
2025-07-25 03:18:45 +0100

    Ajout de l'authentification avec Laravel Breeze et fusion des routes personnalisées

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategorieController;
-use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +13,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/listecategorie',[CategorieController::class,'index'])->name('listecategorie');
-Route::get('/addcategorie',[CategorieController::class,'create'])->name('addcategorie');
-Route::post('/savecategorie',[CategorieController::class,'store'])->name('savecategorie');
-Route::delete('/deletecategorie/{id}',[CategorieController::class,'destroy'])->name('deletecategorie');
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('produit', ProduitController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
