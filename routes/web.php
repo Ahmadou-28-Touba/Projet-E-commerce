2edfac157c8fec93b1c97b8693658ecb7d58408d
Ahmadou Bamba Thiam <thiamahmadoubamba863@gmail.com>
2025-07-26 03:25:47 +0100

    Ajout logout dans la navbar, redirection sur login après déconnexion, et mot de passe oublié via Mailtrap

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/listecategorie', [CategorieController::class,'index'])->name('listecategorie');
+    Route::get('/addcategorie', [CategorieController::class,'create'])->name('addcategorie');
+    Route::post('/savecategorie', [CategorieController::class,'store'])->name('savecategorie');
+    Route::delete('/deletecategorie/{id}', [CategorieController::class,'destroy'])->name('deletecategorie');
+
+    Route::resource('produit', ProduitController::class);
 });
 
-// === CATEGORIES ===
-Route::get('/listecategorie', [CategorieController::class,'index'])->name('listecategorie');
-Route::get('/addcategorie', [CategorieController::class,'create'])->name('addcategorie');
-Route::post('/savecategorie', [CategorieController::class,'store'])->name('savecategorie');
-Route::delete('/deletecategorie/{id}', [CategorieController::class,'destroy'])->name('deletecategorie');
 
-// === PRODUITS ===
-Route::resource('produit', ProduitController::class);
 
 require __DIR__.'/auth.php';
