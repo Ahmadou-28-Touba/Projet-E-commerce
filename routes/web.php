3a6dbac989f6bba4b20a7e6ea28971bf6c194846
Ahmadou Bamba Thiam <thiamahmadoubamba863@gmail.com>
2025-07-26 02:21:17 +0100

    Mise à jour des routes  apres l'authentification!

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ProduitController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,5 +18,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// === CATEGORIES ===
+Route::get('/listecategorie', [CategorieController::class,'index'])->name('listecategorie');
+Route::get('/addcategorie', [CategorieController::class,'create'])->name('addcategorie');
+Route::post('/savecategorie', [CategorieController::class,'store'])->name('savecategorie');
+Route::delete('/deletecategorie/{id}', [CategorieController::class,'destroy'])->name('deletecategorie');
+
+// === PRODUITS ===
+Route::resource('produit', ProduitController::class);
 
 require __DIR__.'/auth.php';
